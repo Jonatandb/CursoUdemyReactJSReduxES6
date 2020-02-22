cadd6986375450594bd6e4afda0ca80eaf4f63c6
Jonatandb <jonatandb@gmail.com>
2020-02-21 23:12:48 -0300

    34. External Font y otros detalles
    https://www.udemy.com/course/react-js-redux-es6-completo-de-0-a-experto-espanol/learn/lecture/8946614#notes

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './styles.css'
 
 const Location = ({ city }) => {
     return (
-        <div>
+        <div className="locationContainer">
             <h1>{city}</h1>
         </div>
     )
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Location from './Location'
 import WeatherData from './WeatherData'
+import './styles.css'
 
 const WeatherLocation = () => {
     return (
-        <div>
+        <div className="weatherLocationContainer">
             <Location city="Buenos Aires" />
             <WeatherData />
         </div>
