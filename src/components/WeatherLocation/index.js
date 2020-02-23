e37b421ceb733827f3346afd31c11d4c74ffb926
Jonatandb <jonatandb@gmail.com>
2020-02-22 23:46:02 -0300

    39. Evento OnClick y manejo de State
    https://www.udemy.com/course/react-js-redux-es6-completo-de-0-a-experto-espanol/learn/lecture/8946636

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -3,7 +3,7 @@ import Location from './Location'
 import WeatherData from './WeatherData'
 import './styles.css'
 import {
-    SUN,
+    SUN, WINDY,
 } from './../../constans/weathers'
 
 const data = {
@@ -13,12 +13,34 @@ const data = {
     wind: "10 km/h"
 }
 
+const data2 = {
+    temperature: 21,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: "40 km/h"
+}
+
 class WeatherLocation extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            city: 'Buenos Aires',
+            data: data,
+        }
+    }
+
+    handleUpdateClick = () => {
+        console.log("Actualizado!")
+        this.setState({ city: 'Barcelona', data: data2 })
+    }
+
     render() {
+        const { city, data } = this.state
         return (
             <div className="weatherLocationContainer">
-                <Location city="Buenos Aires" />
+                <Location city={city} />
                 <WeatherData data={data} />
+                <button onClick={this.handleUpdateClick}>Actializar</button>
             </div>
         )
     }
