485144010fb21efce5c74e99808a37aabc5aca09
Jonatandb <jonatandb@gmail.com>
2020-02-23 00:57:18 -0300

    42. Api Rest OpenWeatherMap
    https://www.udemy.com/course/react-js-redux-es6-completo-de-0-a-experto-espanol/learn/lecture/8946818

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -4,6 +4,23 @@ import WeatherExtraInfo from './WeatherExtraInfo'
 import './styles.css'
 import PropTypes from 'prop-types'
 
+/*
+OpenWeather:	Servicio gratuito que provee datos del clima
+------------
+
+	Página principal:
+					https://openweathermap.org/
+
+	Documentación de la API:
+					https://openweathermap.org/api
+
+	Guía de uso - How to start:
+					https://openweathermap.org/appid
+
+	Ciudad Autónoma de Buenos Aires -> id: 3433955
+
+*/
+
 const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => (
     <div className="weatherDataContainer">
         <WeatherTemperature
