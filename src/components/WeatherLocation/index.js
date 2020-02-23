0828dfbf359511ae5121c22f1c8880421ec94b08
Jonatandb <jonatandb@gmail.com>
2020-02-23 01:34:35 -0300

    43. Fetch y solapa de debugging Network de Chrome
    https://www.udemy.com/course/react-js-redux-es6-completo-de-0-a-experto-espanol/learn/lecture/8946828

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -6,6 +6,29 @@ import {
     SUN, WINDY,
 } from './../../constans/weathers'
 
+/*
+OpenWeather:	Servicio gratuito que provee datos del clima
+------------
+
+	Página principal:
+					https://openweathermap.org/
+
+	Documentación de la API:
+					https://openweathermap.org/api
+
+	Guía de uso - How to start:
+					https://openweathermap.org/appid
+
+	Ciudad Autónoma de Buenos Aires -> id: 3433955
+
+*/
+
+const location = "Buenos Aires,ar"
+const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d"
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather"
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`
+
 const data = {
     temperature: 38,
     weatherState: SUN,
@@ -30,7 +53,30 @@ class WeatherLocation extends Component {
     }
 
     handleUpdateClick = () => {
-        console.log("Actualizado!")
+
+        /* Alternativa trayendo los datos utilizando el id de ciudad
+        como la API sugiere: */
+        /*
+            const locationId = 3433955
+            fetch(`${url_base_weather}?id=${locationId}&appid=${api_key}`)
+        */
+
+        fetch(api_weather)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                if (myJson && myJson.cod && myJson.cod === 200) {
+                    // Logueo en la consola la respuesta desde la API:
+                    console.log(myJson);
+                } else {
+                    console.log("Se produjo un error.", myJson && myJson.message && myJson.message);
+                }
+            })
+            .catch(function (reason) {
+                console.log("Se produjo un error.", reason)
+            });
+
         this.setState({
             city: 'Barcelona',
             data: data2
