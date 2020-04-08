a1dec198dffc6efee2ed7457e33690ce954fb6bd
Jonatandb <jonatandb@gmail.com>
2020-04-07 22:41:24 -0300

    57. Utilización y configuración de ESLint
    https://www.udemy.com/course/react-js-redux-es6-completo-de-0-a-experto-espanol/learn/lecture/8947430#notes

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import Location from "./Location";
-import WeatherData from "./WeatherData";
-import "./styles.css";
-import transformWeather from "../../services/transformWeather";
-import { api_weather } from "../../constans/api_url";
-import { CircularProgress } from "@material-ui/core";
+import React, { Component } from "react"
+import Location from "./Location"
+import WeatherData from "./WeatherData"
+import "./styles.css"
+import transformWeather from "../../services/transformWeather"
+import { api_weather } from "../../constans/api_url"
+import { CircularProgress } from "@material-ui/core"
 
 /*
 OpenWeatherMap:	Servicio gratuito que provee datos del clima
@@ -25,22 +25,22 @@ OpenWeatherMap:	Servicio gratuito que provee datos del clima
 
 class WeatherLocation extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       city: "Buenos Aires",
       data: null,
-    };
+    }
   }
 
   componentDidMount() {
     // Lugar correcto para hacer peticiones al servidor.
-    this.handleUpdateClick();
+    this.handleUpdateClick()
   }
 
   handleUpdateClick = () => {
     fetch(api_weather)
       .then((response) => {
-        return response.json();
+        return response.json()
       })
       .then((weatherJSONResponse) => {
         if (
@@ -50,33 +50,33 @@ class WeatherLocation extends Component {
         ) {
           this.setState({
             data: transformWeather(weatherJSONResponse),
-          });
+          })
         } else {
           console.log(
             "WeatherLocation.handleUpdateClick(): Se produjo un error al obtener datos del servidor.",
             weatherJSONResponse &&
               weatherJSONResponse.message &&
               weatherJSONResponse.message
-          );
+          )
         }
       })
       .catch(function (reason) {
         console.log(
           "WeatherLocation.handleUpdateClick(): Se produjo un error en alguna de las Promises:",
           reason
-        );
-      });
-  };
+        )
+      })
+  }
 
   render() {
-    const { city, data } = this.state;
+    const { city, data } = this.state
     return (
       <div className="weatherLocationContainer">
         <Location city={city} />
         {data ? <WeatherData data={data} /> : <CircularProgress size={60} />}
       </div>
-    );
+    )
   }
 }
 
-export default WeatherLocation;
+export default WeatherLocation
