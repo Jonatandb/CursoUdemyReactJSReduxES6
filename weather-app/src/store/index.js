cf5f645a74ef0e46ca8998fceac1e625737e6cea
Jonatandb <jonatandb@gmail.com>
2020-05-05 22:30:53 -0300

    Fix de typo en el nombre de la función utilizada por Redux DevTools para detectar el store de la aplicación

--- weather-app/src/store/index.js
+++ weather-app/src/store/index.js
@@ -4,6 +4,6 @@ import thunk from 'redux-thunk';
 
 const initialState = { city: '' };
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(reducers.cityReducer, initialState, composeEnhancers(applyMiddleware(thunk)));
